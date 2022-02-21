eb7de747b64caec4816533ddff7c3c38b7746e32
Meidani Pamungkas <68596953+meydanipamungkas@users.noreply.github.com>
2022-02-21 13:54:44 +0700

    Delete MATLAB Code directory

--- MATLAB Code/norm_rute.m
+++
@@ -1,9 +0,0 @@
-function rute1=norm_rute(rute)
-rute1=rute;
-[panjang,lebar]=size(rute1);
-y=zeros(panjang,lebar-1);
-for k=1:panjang
-    y(k,:)=[diff(rute1(k,:)')'];
-    rute1(k,find(y(k,1:end-1)==0)+1)=0;
-end
-end
